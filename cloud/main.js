ee4bad5acaf646d344c2479048a3e9169088244d
Qasim <qasim@krews.com>
2025-01-22 20:20:17 +0400

    CPOR-1609

--- cloud/main.js
+++ cloud/main.js
@@ -264,11 +264,15 @@ Parse.Cloud.define('deleteSectionImage', async (request) => {
     // Assuming 'images' is an array field in the 'Sections' class
     let images = section.get('images') || [];
 
+    console.log('images');
+    console.log(images);
     // let filteredImages = images.filter(image => image.name != imageId);
     let filteredImages = images.filter((child) => {
       return child.name != imageId;
     });
 
+    console.log('filteredImages');
+    console.log(filteredImages);
 
     // Update the section with the new images array
     section.set('images', filteredImages);
